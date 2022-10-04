81b33eab7868c6831d813c7c567b4f2c27b677c9
forlinguistics <42992800+forlinguistics@users.noreply.github.com>
2022-10-04 18:06:54 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,83 @@
+"""TicTac game"""
+import re
+
+
+class TicTacGame:
+    """TicTac game class"""
+    reference = """
+     1 2 3
+    a | |
+     -+-+-
+    b | |
+     -+-+-
+    c | |
+    """
+    player_switch = True
+    row_dict = {'A': 0, 'B': 1, 'C': 2}
+
+    def __init__(self):
+        self.board = [[' '] * 3 for _ in range(3)]
+
+    def show_board(self):
+        """prints current board state"""
+        print('\n-+-+-\n'.join(['|'.join(row) for row in self.board]))
+
+    def validate_input(self, inp):
+        """validates user input and changes the board state if it is correct"""
+        move_pattern = re.compile('[A-C] [1-3]')
+        if not move_pattern.match(inp):
+            print(r'Input should have the valid format(Example "A 2")')
+            return True
+        row, col = inp.split()
+        row = self.row_dict[row]
+        col = int(col) - 1
+        if self.board[row][col] != ' ':
+            print('Field is not empty')
+            return True
+        if self.player_switch:
+            self.board[row][col] = 'x'
+        else:
+            self.board[row][col] = 'o'
+        self.player_switch = not self.player_switch
+        return False
+
+    def check_winner(self):
+        """checks winner from current board state"""
+        for i in range(3):
+            if self.board[i][0] != ' ' and self.board[i][0] == self.board[i][1] == self.board[i][2]:
+                return True, self.board[i][0]
+
+            if self.board[0][i] != ' ' and self.board[0][i] == self.board[1][i] == self.board[2][i]:
+                return True, self.board[0][i]
+
+        if self.board[0][0] != ' ' and self.board[0][0] == self.board[1][1] == self.board[2][2]:
+            return True, self.board[0][0]
+
+        if self.board[0][2] != ' ' and self.board[0][2] == self.board[1][1] == self.board[2][0]:
+            return True, self.board[0][2]
+
+        return False, ' '
+
+    def start_game(self):
+        """game method"""
+        move_cnt = 0
+        print("Game started: to make a move enter position coordinates (reference below).")
+        print(self.reference)
+        while True:
+            print("Make a move: ")
+            move = input()
+            while self.validate_input(move):
+                print("Make a move: ")
+                move = input()
+            self.show_board()
+            winner = self.check_winner()
+            if winner[0]:
+                return f'Game ended. Winner is {winner[1]}'
+            move_cnt += 1
+            if move_cnt == 9:
+                return 'Game ended. Draw'
+
+
+if __name__ == '__main__':
+    game = TicTacGame()
+    print(game.show_board())
---
+++ tests.py
@@ -0,0 +1,60 @@
+import unittest
+import io
+from unittest.mock import patch
+from main import TicTacGame
+
+EMPTY_OUTP = " | | \n-+-+-\n | | \n-+-+-\n | | \n"
+FILLED_OUTP = " |x| \n-+-+-\n | | \n-+-+-\n | | \n"
+
+
+class TicTacTestCase(unittest.TestCase):
+    win_moves = ['B 1', 'A 1', 'B 2', 'A 2', 'B 3']
+    draw_moves = ['A 1', 'B 2', 'A 2', 'A 3', 'C 1', 'C 2', 'C 3', 'B 1', 'B 3']
+
+    def test_validate_inp(self):
+        game = TicTacGame()
+        assert game.validate_input('A.2')
+        assert game.validate_input('A 4')
+        assert game.validate_input('D 2')
+        assert not game.validate_input('A 2')
+        assert game.board[0][1] == 'x'
+        assert game.validate_input('A 2')
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_show_board(self, mock_stdout):
+        game = TicTacGame()
+        game.show_board()
+        self.assertEqual(mock_stdout.getvalue(), EMPTY_OUTP)
+        game.validate_input('A 2')
+        game.show_board()
+        self.assertEqual(mock_stdout.getvalue(), EMPTY_OUTP + FILLED_OUTP)
+
+    def test_check_winner(self):
+        hor_win = ['B 1', 'A 1', 'B 2', 'A 2', 'B 3']
+        vert_win = ['A 2', 'A 3', 'B 2', 'B 3', 'C 1', 'C 3']
+        diag_win = ['A 3', 'A 1', 'B 2', 'B 1', 'C 1']
+        game = TicTacGame()
+        for i in hor_win:
+            game.validate_input(i)
+        assert (game.check_winner() == (True, 'x'))
+        game = TicTacGame()
+        for i in vert_win:
+            game.validate_input(i)
+        game.show_board()
+        assert (game.check_winner() == (True, 'o'))
+        game = TicTacGame()
+        for i in diag_win:
+            game.validate_input(i)
+        assert (game.check_winner() == (True, 'x'))
+        game = TicTacGame()
+        game.validate_input('B 2')
+        assert (game.check_winner() == (False, ' '))
+
+    @patch('builtins.input', side_effect=win_moves)
+    def test_start_game_win(self, mock_inputs):
+        game = TicTacGame()
+        assert game.start_game() == 'Game ended. Winner is x'
+    @patch('builtins.input', side_effect=draw_moves)
+    def test_start_game_draw(self, mock_inputs):
+        game = TicTacGame()
+        assert game.start_game() == 'Game ended. Draw'
